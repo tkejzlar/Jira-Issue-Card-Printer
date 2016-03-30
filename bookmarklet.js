d84485a189fb16a6fad94a0b0d0dc59b01368997
rmx@rmx.ro <Parola_mea@githab1>
2016-03-30 11:10:14 +0200

    google analytics patch

--- bookmarklet.js
+++ bookmarklet.js
@@ -101,7 +101,6 @@
     var promises = [];
 
     console.log("Init...")
-    initGoogleAnalytics();
 
     addStringFunctions();
     loadSettings();
@@ -556,36 +555,6 @@
     return result;
   }
 
-  //############################################################################################################################
-  //############################################################################################################################
-  //############################################################################################################################
-
-  function initGoogleAnalytics() {
-    if (global.isDev) {
-      this.ga = function(){ console.log("GoogleAnalytics: " + Object.keys(arguments).map(key => arguments[key]))}
-      return;
-    }
-    // <GoogleAnalytics>
-    (function(i, s, o, g, r, a, m) {
-      i['GoogleAnalyticsObject'] = r;
-      i[r] = i[r] || function() {
-        (i[r].q = i[r].q || []).push(arguments)
-      }, i[r].l = 1 * new Date();
-      a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0];
-      a.async = 1;
-      a.src = g;
-      m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-    ga('create', 'UA-50840116-3', {
-      'alwaysSendReferrer': true
-    });
-    ga('set', 'page', '/cardprinter');
-  }
-
-  //############################################################################################################################
-  //############################################################################################################################
   //############################################################################################################################
 
   function parseBool(text, def){

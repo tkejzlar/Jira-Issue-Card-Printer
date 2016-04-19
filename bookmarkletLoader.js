e32831e08c4272e4955e08e24b3dbad8dab7b0b9
tkejzlar <tomas.kejzlar@gmail.com>
2016-04-19 10:36:35 +0200

    Update bookmarkletLoader.js

--- bookmarkletLoader.js
+++ bookmarkletLoader.js
@@ -2,7 +2,7 @@
 javascript:(function(){
         var head = document.getElementsByTagName("head")[0];
         var scriptElement = document.createElement("script");
-        scriptElement.src = 'https://rmxro.github.io/Jira-Issue-Card-Printer/bookmarklet.js';
+        scriptElement.src = 'https://tkejzlar.github.io/Jira-Issue-Card-Printer/bookmarklet.js';
         head.appendChild(scriptElement);
         head.removeChild(scriptElement);
 })();
